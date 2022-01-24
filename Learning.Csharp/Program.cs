1f845d9b54006683d339f9742ce9fa451000c91c
AYISAT MUSTAPHA <abiodunayisat2@gmail.com>
2022-01-24 23:33:09 +0100

    learning

--- Learning.Csharp/Program.cs
+++ Learning.Csharp/Program.cs
@@ -6,7 +6,8 @@ namespace Learning.Csharp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Aisha!");
+
         }
     }
 }
